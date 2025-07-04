9178960774f7fe324050dd5326940b9e45d48473
Jimmy Robinson <robinson.jimmy@hotmail.com>
2025-07-05 10:28:03 +1200

    Added screenshot functionality

--- src/step-definitions/hooks/hooks.ts
+++ src/step-definitions/hooks/hooks.ts
@@ -1,4 +1,4 @@
-import { After, AfterAll, Before, BeforeAll, context } from "@cucumber/cucumber";
+import { After, AfterAll, Before, BeforeAll, context, Status } from "@cucumber/cucumber";
 import { Browser, chromium,  } from "@playwright/test"
 import { pageFixture } from "./browserContectFixture"
 
@@ -23,7 +23,21 @@ Before(async function() {
 
 })
 
-After(async function() {
+After(async function({pickle, result}) {
+    if (result?.status === Status.FAILED) {
+        if(pageFixture.page) {
+            const screenshotPath = `./reports/screenshots/${pickle.name}-${Date.now()}.png`;
+            const image = await pageFixture.page.screenshot({
+                path: screenshotPath,
+                type: 'png',
+                //timeout: 60000
+            });
+            await this.attach(image, 'image/png');
+        } else {
+            console.error('pageFixture.page is undefined');
+        }
+
+    }
     await pageFixture.context.close();
     await browser.close();
 })
